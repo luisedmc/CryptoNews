ae881aee6aa48d9154a4978fc6f26241915f2909
LuisEduardo-M <luiseduardomc019@gmail.com>
2023-01-28 23:55:33 -0300

    changed folder structure

---
+++ src/pages/About.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Title from "../components/Title";
+import { Row } from "react-bootstrap";
+
+const About = () => {
+    return (
+        <>
+            <Title title="About" />
+
+            <Row>
+                <h2>Purpose of the Project:</h2>
+                <p>
+                    This project was created to use public external APIs to display cryptocurrencies data and articles about.
+                </p>
+            </Row>
+
+            <Row>
+                <h2>Technologies:</h2>
+                <ul>
+                    <li>React</li>
+                    <li>React Bootstrap</li>
+                    <li>React Router</li>
+                    <li>React Hooks</li>
+                </ul>
+            </Row>
+        </>
+    );
+}
+
+export default About;
