bc52e8cdae557b4ed469bf2f8d4806740846a89c
LuisEduardo-M <luiseduardomc019@gmail.com>
2023-02-02 20:56:49 -0300

    added the search box

--- src/pages/Cryptos.jsx
+++ src/pages/Cryptos.jsx
@@ -3,11 +3,13 @@ import { Link } from 'react-router-dom';
 import { Row, Col, ListGroup, Badge } from 'react-bootstrap';
 import Loading from "../components/Loading";
 import Title from '../components/Title';
+import SearchBox from '../components/SearchBox';
 
 const COIN_KEY = process.env.REACT_APP_COIN_KEY;
 
 function Crypto() {
     const [cryptos, setCryptos] = useState([]);
+    const [searchCrypto, setSearchCrypto] = useState("");
     const [isLoading, setIsLoading] = useState(false);
 
     const cryptoPriceFormatter = apiPrice => {
@@ -41,30 +43,59 @@ function Crypto() {
             .catch(error => console.log(error));
     }, []);
 
+    const handleSearch = event => {
+        setSearchCrypto(event.target.value);
+    }
+
+    const searchedCrypto = cryptos.filter(crypto => {
+        return crypto.name.toLowerCase().includes(searchCrypto.toString().toLowerCase());
+    });
+
     return (
         <>
             <Title title="Cryptocurrencies" />
 
+            <SearchBox onChange={handleSearch} className={"mt-3 mb-3"} />
             {isLoading && <Loading />}
+            {searchCrypto !== "" ? (
+                searchedCrypto.map((crypto, index) => (
+                    <ListGroup key={index}>
+                        <ListGroup.Item action>
+                            <Link to={`/cryptos/${crypto.asset_id}`} className="text-decoration-none">
+                                <Row className="align-items-center">
+                                    <Col>
+                                        {crypto.name}
+                                    </Col>
 
-            <ListGroup>
-                {cryptos.map(crypto => (
-                    <ListGroup.Item action key={crypto.asset_id}>
-                        <Link to={`/cryptos/${crypto.asset_id}`} className="text-decoration-none">
-                            <Row className="align-items-center">
-                                <Col>
-                                    {crypto.name}
-                                </Col>
+                                    <Col className="d-flex justify-content-end">
+                                        <Badge bg="success">USD: <small>{cryptoPriceFormatter(crypto.price_usd)}</small>
+                                        </Badge>
+                                    </Col>
+                                </Row>
+                            </Link>
+                        </ListGroup.Item>
+                    </ListGroup>
+                ))
+            ) : (
+                <ListGroup>
+                    {cryptos.map(crypto => (
+                        <ListGroup.Item action key={crypto.asset_id}>
+                            <Link to={`/cryptos/${crypto.asset_id}`} className="text-decoration-none">
+                                <Row className="align-items-center">
+                                    <Col>
+                                        {crypto.name}
+                                    </Col>
 
-                                <Col className="d-flex justify-content-end">
-                                    <Badge bg="success">USD: <small>{cryptoPriceFormatter(crypto.price_usd)}</small>
-                                    </Badge>
-                                </Col>
-                            </Row>
-                        </Link>
-                    </ListGroup.Item>
-                ))}
-            </ListGroup>
+                                    <Col className="d-flex justify-content-end">
+                                        <Badge bg="success">USD: <small>{cryptoPriceFormatter(crypto.price_usd)}</small>
+                                        </Badge>
+                                    </Col>
+                                </Row>
+                            </Link>
+                        </ListGroup.Item>
+                    ))}
+                </ListGroup>
+            )}
         </>
     );
 }
