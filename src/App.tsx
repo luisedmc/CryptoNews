6967b8b4d87289a823aca35e98de177a69da24f8
LuisEduardo-M <luiseduardomc019@gmail.com>
2023-02-05 03:47:01 -0300

    app tsc

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import React from "react"
+import { Link, Outlet } from "react-router-dom"
+import { Container, Row, Col, Nav } from "react-bootstrap"
+import "./index.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+
+export const App = () => {
+  return (
+    <Container className="mt-5">
+      <Row>
+        <Col md={2}>
+          <Nav className="list-group pe-auto">
+            <Link to="/" className="list-group-item list-group-item-action">
+              Home
+            </Link>
+            <Link to="/cryptos" className="list-group-item list-group-item-action">
+              Cryptos
+            </Link>
+            <Link to="/articles" className="list-group-item list-group-item-action">
+              Articles
+            </Link>
+            <Link to="/about" className="list-group-item list-group-item-action">
+              About
+            </Link>
+          </Nav>
+        </Col>
+
+        <Col md={10}>
+          <Outlet />
+        </Col>
+      </Row>
+    </Container>
+  )
+}
